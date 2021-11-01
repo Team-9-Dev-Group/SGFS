7dfc7a4e6674744f1d2fd4dc85b7400f35de31ed
Madhur_Gupta <madleads2001@gmail.com>
2021-11-01 09:59:56 +0530

    Completes migration of achievementsArrayList and its methods from Semester -> Student

--- src/Semester.java
+++ src/Semester.java
@@ -1,15 +1,15 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Semester {
 
-    int year;
-    String semester;
-    String department;
+    private int year;
+    private String semester;
+    private String department;
 
-    ArrayList<Course> CoursesList = new ArrayList<Course>();
-    ArrayList<achievement> achievementsArrayList = new ArrayList<achievement>();
-    LinkedList<Exam> examLinkedList = new LinkedList<Exam>();
-    
+    private ArrayList<Course> CoursesList = new ArrayList<Course>();
+    private LinkedList<Exam> examLinkedList = new LinkedList<Exam>();
+
     //Getters and setters
     public void setYear(int year) {
         this.year = year;
@@ -35,152 +35,91 @@ public class Semester {
         return department;
     }
 
-    //Adding objects to achievementsArrayList
+    // Course List method
 
-    public void addAchievement(String name){
-        achievement Achievement= new achievement(name);
-        //TODO: add something according to Achievements class
-        achievementsArrayList.add(Achievement);
-    }
-    public void addAchievement(String name,Date date)
-    {
-        achievement Achievement = new achievement(name,date);
-        achievementsArrayList.add(Achievement);
-    }
-    public void addAchievement(String name,Date date,String type)
-    {
-        achievement Achievement = new achievement(name,date,type);
-        achievementsArrayList.add(Achievement);
-    }
-    public void removeAchievement(String Name)
-    {
-        for(int i=0;i<achievementsArrayList.size();i++)
-        {
-            if(achievementsArrayList.get(i).titleOfAchievement ==Name)
-            {
-                achievementsArrayList.remove(i);
-                break;
-            }
-        }
-    }
-    public void removeAchievement(String Name, Date date)
-    {
-        for(int i=0;i<achievementsArrayList.size();i++)
-        {
-            if(achievementsArrayList.get(i).titleOfAchievement ==Name && achievementsArrayList.get(i).achievementDate==date)
-            {
-                achievementsArrayList.remove(i);
-                break;
-            }
-        }
-    }
-    
-    public boolean modifyAchievement(String name,Date date,String type, String worth)
-    {
-        //searching with name and modifying other details
-        for(int i=0;i<achievementsArrayList.size();i++)
-        {
-            if(achievementsArrayList.get(i).titleOfAchievement==name)
-            {
-                achievementsArrayList.get(i).setAchievementInfo(date, type, worth);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    
-    // Course List method 
-    
     static String str = "Optional Course";
-    
-    static String[] Sem_1_Courses = new String[] {"MA101","PH100","PH160","IT101","IT161","EC100","EC160","HS101"};
-    static String[] Sem_2_Courses = new String[] {"MA102","PH110","PH170","EE100","EE160","CS102","CS162","HS102"};
-    static String[] Sem_3_Courses = new String[] {"SC201","MA201","HS201","CS201","CS261","CS203","CS263","EC201","EC261"};
-    static String[] Sem_4_Courses_CSE = new String[] {"MA202","HS202","CS202","CS204","CS262","CS206","CS266","CS208","CS268"};
-    static String[] Sem_4_Courses_IT = new String[] {"HS202","IT202","IT262","CS204","CS262","CS206","CS266","CS208","CS268"};
-    static String[] Sem_5_Courses_CSE = new String[] {"CS301","CS361","CS303","CS363","CS305",str,str,"CS391"};
-    static String[] Sem_5_Courses_IT = new String[] {"CS301","CS361","CS303","CS363","IT301","IT361",str,str,"IT391"};
-    static String[] Sem_6_Courses_CSE = new String[] {"CS302","CS362","CS304","CS364",str,str,str,str};
-    static String[] Sem_6_Courses_IT = new String[] {"IT302","IT362","IT304",str,str,str,str};
-    static String[] Sem_7_Courses_CSE = new String[] {"CS401","CS461",str,str,str,str,"CS491"};
-	static String[] Sem_7_Courses_IT = new String[] {"IT401","IT461",str,str,str,str,"IT491"};
-	static String[] Sem_8_Courses = new String[] {"Project"};
-    
-    private void set_CourseList( String[] courses )    
-    {
-    	for ( int i = 0 ; i < courses.length ; i++ )
-    	{	
-    		Course name = new Course();
-    		name.set_Code(courses[i]);
-    		CoursesList.add(name);
-    	}
-    }
-    
-    public void create_CourseList( int year , String semester , String department )
-    {
-    	int Sem_No = 0 ;
-		switch ( semester )
-		{
-			case "Autumn" :
-				Sem_No = (year * 2) - 1 ;
-				break;
-			case "Winter" :
-				Sem_No = year * 2 ;
-				break;
-			default :
-			     System.out.println("Invalid Semester.");
-		}
-		
-		switch ( Sem_No )
-		{
-			case 1 :
-				set_CourseList(Sem_1_Courses);
-				break;
-			case 2 :
-				set_CourseList(Sem_2_Courses);
-				break;
-			case 3 :
-				set_CourseList(Sem_3_Courses);
-				break;
-			case 4 :
-				if ( department == "CSE" )
-				set_CourseList(Sem_4_Courses_CSE);
-				else if ( department == "IT" )
-				set_CourseList(Sem_4_Courses_IT);
-				break;
-			case 5 :
-				if ( department == "CSE" )
-				set_CourseList(Sem_5_Courses_CSE);
-				else if ( department == "IT" )
-				set_CourseList(Sem_5_Courses_IT);
-				break;
-			case 6 :
-				if ( department == "CSE" )
-				set_CourseList(Sem_6_Courses_CSE);
-				else if ( department == "IT" )
-				set_CourseList(Sem_6_Courses_IT);
-				break;
-			case 7 :
-				if ( department == "CSE" )
-				set_CourseList(Sem_7_Courses_CSE);
-				else if ( department == "IT" )
-				set_CourseList(Sem_7_Courses_IT);
-				break;
-			case 8 :
-				set_CourseList(Sem_8_Courses);
-				break;
 
-			default :
-				System.out.println("Invalid Input");
-				
-		}
+    static String[] Sem_1_Courses = new String[]{"MA101", "PH100", "PH160", "IT101", "IT161", "EC100", "EC160", "HS101"};
+    static String[] Sem_2_Courses = new String[]{"MA102", "PH110", "PH170", "EE100", "EE160", "CS102", "CS162", "HS102"};
+    static String[] Sem_3_Courses = new String[]{"SC201", "MA201", "HS201", "CS201", "CS261", "CS203", "CS263", "EC201", "EC261"};
+    static String[] Sem_4_Courses_CSE = new String[]{"MA202", "HS202", "CS202", "CS204", "CS262", "CS206", "CS266", "CS208", "CS268"};
+    static String[] Sem_4_Courses_IT = new String[]{"HS202", "IT202", "IT262", "CS204", "CS262", "CS206", "CS266", "CS208", "CS268"};
+    static String[] Sem_5_Courses_CSE = new String[]{"CS301", "CS361", "CS303", "CS363", "CS305", str, str, "CS391"};
+    static String[] Sem_5_Courses_IT = new String[]{"CS301", "CS361", "CS303", "CS363", "IT301", "IT361", str, str, "IT391"};
+    static String[] Sem_6_Courses_CSE = new String[]{"CS302", "CS362", "CS304", "CS364", str, str, str, str};
+    static String[] Sem_6_Courses_IT = new String[]{"IT302", "IT362", "IT304", str, str, str, str};
+    static String[] Sem_7_Courses_CSE = new String[]{"CS401", "CS461", str, str, str, str, "CS491"};
+    static String[] Sem_7_Courses_IT = new String[]{"IT401", "IT461", str, str, str, str, "IT491"};
+    static String[] Sem_8_Courses = new String[]{"Project"};
+
+    private void set_CourseList(String[] courses) {
+        for (int i = 0; i < courses.length; i++) {
+            Course name = new Course();
+            name.set_Code(courses[i]);
+            CoursesList.add(name);
+        }
     }
-    
-    
-    
+
+    public void create_CourseList(int year, String semester, String department) {
+        int Sem_No = 0;
+        switch (semester) {
+            case "Autumn":
+                Sem_No = (year * 2) - 1;
+                break;
+            case "Winter":
+                Sem_No = year * 2;
+                break;
+            default:
+                System.out.println("Invalid Semester.");
+        }
+
+        switch (Sem_No) {
+            case 1:
+                set_CourseList(Sem_1_Courses);
+                break;
+            case 2:
+                set_CourseList(Sem_2_Courses);
+                break;
+            case 3:
+                set_CourseList(Sem_3_Courses);
+                break;
+            case 4:
+                if (department == "CSE")
+                    set_CourseList(Sem_4_Courses_CSE);
+                else if (department == "IT")
+                    set_CourseList(Sem_4_Courses_IT);
+                break;
+            case 5:
+                if (department == "CSE")
+                    set_CourseList(Sem_5_Courses_CSE);
+                else if (department == "IT")
+                    set_CourseList(Sem_5_Courses_IT);
+                break;
+            case 6:
+                if (department == "CSE")
+                    set_CourseList(Sem_6_Courses_CSE);
+                else if (department == "IT")
+                    set_CourseList(Sem_6_Courses_IT);
+                break;
+            case 7:
+                if (department == "CSE")
+                    set_CourseList(Sem_7_Courses_CSE);
+                else if (department == "IT")
+                    set_CourseList(Sem_7_Courses_IT);
+                break;
+            case 8:
+                set_CourseList(Sem_8_Courses);
+                break;
+
+            default:
+                System.out.println("Invalid Input");
+
+        }
+    }
+
+
     //Methods for Exam Class
-    
+
     public void listExams() {
         // Right now it will just print out the examLinkedList
         for (Exam exam : examLinkedList) {
@@ -204,28 +143,28 @@ public class Semester {
         }
         return false; // exam not found
     }
-    
-     public void addExam(String Course, String Date, String Exam_type, String Schedule, double Hrs_req, double Max_marks, double Contribution) {
-        Exam newExam = new Exam(Course, Date, Exam_type,Schedule, Hrs_req, Max_marks, Contribution);
+
+    public void addExam(String Course, String Date, String Exam_type, String Schedule, double Hrs_req, double Max_marks, double Contribution) {
+        Exam newExam = new Exam(Course, Date, Exam_type, Schedule, Hrs_req, Max_marks, Contribution);
         examLinkedList.add(newExam);
     }
-    
+
     public void addExam(String Course, String Date) {
         Exam newExam = new Exam(Course, Date);
         examLinkedList.add(newExam);
     }
 
     public void removeExam(String Course, String Date) {
-        for (Exam i:examLinkedList ) {
-            if(i.Date.equals(Date) && i.Course.equalsIgnoreCase(Course)) {
+        for (Exam i : examLinkedList) {
+            if (i.Date.equals(Date) && i.Course.equalsIgnoreCase(Course)) {
                 examLinkedList.remove(i);
             }
         }
     }
 
     public void removeExam(String Date) {
-        for (Exam i:examLinkedList ) {
-            if(i.Date.equals(Date)) {
+        for (Exam i : examLinkedList) {
+            if (i.Date.equals(Date)) {
                 examLinkedList.remove(i);
             }
         }
--- src/Student.java
+++ src/Student.java
@@ -1,54 +1,107 @@
-public class Student{
-	private String name;
-	private String StudentId;
-	private Boolean isUG;
-	private String phone_no;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Student {
+    private String name;
+    private String StudentId;
+    private Boolean isUG;
+    private String phone_no;
     private String Probation;
-    
-	public Students(String name, String StudentId, Boolean isUG, String phone_no, String probation) {
-	
-		this.name = name;
-		this.StudentId = StudentId;
-		this.isUG = isUG;
-		this.phone_no = phone_no;
-		this.Probation = Probation;
-	}
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    private ArrayList<achievement> achievementsArrayList = new ArrayList<achievement>();
 
-	public String getStudentId() {
-		return StudentId;
-	}
+    public Students(String name, String StudentId, Boolean isUG, String phone_no, String probation) {
 
-	public void setStudentId(String studentId) {
-		StudentId = studentId;
-	}
+        this.name = name;
+        this.StudentId = StudentId;
+        this.isUG = isUG;
+        this.phone_no = phone_no;
+        this.Probation = Probation;
+    }
 
-	public Boolean getIsUG() {
-		return isUG;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setIsUG(Boolean isUG) {
-		this.isUG = isUG;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getPhone_no() {
-		return phone_no;
-	}
+    public String getStudentId() {
+        return StudentId;
+    }
 
-	public void setPhone_no(String phone_no) {
-		this.phone_no = phone_no;
-	}
-	public String getProbation() {
-		return Probation;
-	}
+    public void setStudentId(String studentId) {
+        StudentId = studentId;
+    }
 
-	public void setProbation(String probation) {
-		this.Probation = probation;
-	}
+    public Boolean getIsUG() {
+        return isUG;
+    }
+
+    public void setIsUG(Boolean isUG) {
+        this.isUG = isUG;
+    }
+
+    public String getPhone_no() {
+        return phone_no;
+    }
+
+    public void setPhone_no(String phone_no) {
+        this.phone_no = phone_no;
+    }
+
+    public String getProbation() {
+        return Probation;
+    }
+
+    public void setProbation(String probation) {
+        this.Probation = probation;
+    }
+
+    //Adding objects to achievementsArrayList
+    public void addAchievement(String name) {
+        achievement Achievement = new achievement(name);
+        //TODO: add something according to Achievements class
+        achievementsArrayList.add(Achievement);
+    }
+
+    public void addAchievement(String name, Date date) {
+        achievement Achievement = new achievement(name, date);
+        achievementsArrayList.add(Achievement);
+    }
+
+    public void addAchievement(String name, Date date, String type) {
+        achievement Achievement = new achievement(name, date, type);
+        achievementsArrayList.add(Achievement);
+    }
+
+    public void removeAchievement(String Name) {
+        for (int i = 0; i < achievementsArrayList.size(); i++) {
+            if (achievementsArrayList.get(i).titleOfAchievement == Name) {
+                achievementsArrayList.remove(i);
+                break;
+            }
+        }
+    }
+
+    public void removeAchievement(String Name, Date date) {
+        for (int i = 0; i < achievementsArrayList.size(); i++) {
+            if (achievementsArrayList.get(i).titleOfAchievement == Name && achievementsArrayList.get(i).achievementDate == date) {
+                achievementsArrayList.remove(i);
+                break;
+            }
+        }
+    }
+
+    public boolean modifyAchievement(String name, Date date, String type, String worth) {
+        //searching with name and modifying other details
+        for (int i = 0; i < achievementsArrayList.size(); i++) {
+            if (achievementsArrayList.get(i).titleOfAchievement == name) {
+                achievementsArrayList.get(i).setAchievementInfo(date, type, worth);
+                return true;
+            }
+        }
+        return false;
+    }
