4329d2063bfeaa5b4d7151f37e343c94b0b7b4d5
TusharMaithani-2002 <84312693+TusharMaithani-2002@users.noreply.github.com>
2021-12-28 09:40:07 +0530

    added constructor

--- src/GymKhana.java
+++ src/GymKhana.java
@@ -1,6 +1,10 @@
 public class GymKhana {
     
     public String nameOfCollege;
+    
+    public GymKhana (String nameOfCollege) {
+        this.nameOfCollege = nameOfCollege;
+    }
 
     public void setNameOfCollege(String nameOfCollege) {
         this.nameOfCollege = nameOfCollege;
