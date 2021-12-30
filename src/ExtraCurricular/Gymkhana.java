f66fda56a04a5be5057308f11f2af810022baa0c
RiyaParikh0112 <moodycodes@gmail.com>
2021-12-30 11:29:30 +0530

    Gymkhana and Committee added

---
+++ src/ExtraCurricular/Gymkhana.java
@@ -0,0 +1,13 @@
+package ExtraCurricular;
+
+public class Gymkhana {
+    public String nameOfCollege;
+
+    public void setNameOfCollege(String nameOfCollege) {
+        this.nameOfCollege = nameOfCollege;
+    }
+
+    public String getNameOfCollege() {
+        return this.nameOfCollege;
+    }
+}
