4bbe21763a6d2a7b1cee9bfbea8204ff253a2b44
TusharMaithani-2002 <84312693+TusharMaithani-2002@users.noreply.github.com>
2021-12-30 11:02:04 +0530

    Create ClubInterface.java

---
+++ src/ExtraCurricular/ClubInterface.java
@@ -0,0 +1,11 @@
+public interface ClubInterface {
+    public void setEvent(String event);
+    public void setNameOfClub(String nameOfClub);
+    public void setNameOfSecretary(String nameOfSecretary);
+    public void setNameOfJointSecretary(String nameOfViceSecretary);
+    public String getNameOfClub();
+    public String getNameOfSecretary();
+    public String getNameOfViceSecretary();
+    public void printClubEvents();
+    public void removeEvent(String event);
+}
